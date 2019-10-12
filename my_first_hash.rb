e009d7a655935863e779c5fde9d1c262ebb6dbde
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-12 19:45:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
 
   # add 4 gun powder to the shipping_manifest hash below
 
